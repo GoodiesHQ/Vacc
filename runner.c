76dcc224426b18c9369e6e53f0850a257597df55
GoodiesHQ <goodies@protonmail.com>
2017-04-17 00:09:38 -0700

    changed runner.c

--- runner.c
+++ runner.c
@@ -7,8 +7,7 @@
 int main(int argc, char **argv)
 {
 
-    const unsigned char shellcode[] = \
-                                      "\x55\x89\xe5\x31\xd2\xb6\x0a\xc1\xe2\x08\x66\xba\x65\x72\x52\x68\x6e\x69\x67\x67\x68\x20\x75\x70\x20\x68\x53\x68\x75\x74\x31\xc0\xb0\x04\x31\xdb\x43\x89\xe1\x31\xd2\xb2\x0f\xcd\x80\x89\xec\x5d\xc3";
+    const unsigned char shellcode[] ="";
 
     void *addr = mmap(NULL, sizeof(shellcode), PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0);
     if(addr == MAP_FAILED)
